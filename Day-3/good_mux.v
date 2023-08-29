5e9ee22752dbe81a48b76c1154bbb4e7248be4e0
Tawfeeq khan <142083027+Tawfeeq2507@users.noreply.github.com>
2023-08-29 20:14:37 +0530

    Update good_mux.v

--- Day-3/good_mux.v
+++ Day-3/good_mux.v
@@ -1,1 +1,13 @@
+`vim good_mux.v`
+``` verilog
 
+module good_mux (input i0 , input i1 , input sel , output reg y);
+always @ (*)
+begin
+        if(sel)
+                y <= i1;
+        else
+                y <= i0;
+end
+endmodule                 
+```

656f2e1e294d8c9583c44ffbac997564a0cd4f4c
Tawfeeq khan <142083027+Tawfeeq2507@users.noreply.github.com>
2023-09-03 19:49:49 +0530

    Create dff_syncres.v

---
+++ Day-4/dff_syncres.v
@@ -0,0 +1,12 @@
+`vim dff_syncres.v`
+``` verilog
+odule dff_syncres ( input clk , input async_reset , input sync_reset , input d , output reg q );
+always @ (posedge clk )
+begin
+        if (sync_reset)
+                q <= 1'b0;
+        else
+                q <= d;
+end
+endmodule
+```

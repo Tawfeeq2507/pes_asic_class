7e2ea53f14a7993b601c09fdcf5b9252859f0018
Tawfeeq khan <142083027+Tawfeeq2507@users.noreply.github.com>
2023-09-03 22:22:18 +0530

    Create counter_opt.v

---
+++ Day-5/counter_opt.v
@@ -0,0 +1,16 @@
+vim counter_opt.v
+``` verilog
+module counter_opt (input clk , input reset , output q);
+reg [2:0] count;
+assign q = count[0];
+
+always @(posedge clk ,posedge reset)
+begin
+        if(reset)
+                count <= 3'b000;
+        else
+                count <= count + 1;
+end
+
+endmodule
+```

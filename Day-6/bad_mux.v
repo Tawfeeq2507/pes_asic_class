69ba1160acf4a647262c183c1df50dada93d829e
Tawfeeq khan <142083027+Tawfeeq2507@users.noreply.github.com>
2023-09-04 00:00:31 +0530

    Create bad_mux.v

---
+++ Day-6/bad_mux.v
@@ -0,0 +1,12 @@
+vim bad_mux.v
+``` verilog
+module bad_mux (input i0 , input i1 , input sel , output reg y);
+always @ (sel)
+begin
+        if(sel)
+                y <= i1;
+        else
+                y <= i0;
+end
+endmodule
+```
